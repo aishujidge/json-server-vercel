0467088fb3d1e9713adec9be46ec822d184fa71c
Danish2511 <danish.shaikh3628@gmail.com>
2024-09-28 21:35:35 +0530

    added proper response messages

--- api/server.js
+++ api/server.js
@@ -1,32 +1,42 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-
-const server = jsonServer.create()
-
-// Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
-
-// Comment out to allow write operations
-// const router = jsonServer.router('db.json')
-
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
 server.use(jsonServer.rewriter({
-    '/ims/api/users': '/users', // GET all users, POST add user
-    '/ims/api/users/:id': '/users/:id', // GET user by ID, PUT update user, DELETE delete user
-}))
-
-server.use(router)
+    '/ims/api/users': '/users',
+    '/ims/api/users/:id': '/users/:id',
+}));
+router.render = (req, res) => {
+    const method = req.method;
+    const resource = 'user';
+    if (method === 'POST' && res.statusCode === 201) {
+        res.jsonp({
+            message: `${resource.charAt(0).toUpperCase() + resource.slice(1)} created successfully`,
+            // data: res.locals.data
+        });
+    } else if (method === 'PUT' && res.statusCode === 200) {
+        res.jsonp({
+            message: `${resource.charAt(0).toUpperCase() + resource.slice(1)} updated successfully`,
+            // data: res.locals.data
+        });
+    } else if (method === 'DELETE' && res.statusCode === 200) {
+        res.jsonp({
+            message: `${resource.charAt(0).toUpperCase() + resource.slice(1)} deleted successfully`,
+        });
+    } else {
+        res.jsonp({
+            // data: res.locals.data
+        });
+    }
+    if (res.statusCode === 404) {
+        res.status(404).jsonp({
+            message: `${resource.charAt(0).toUpperCase() + resource.slice(1)} not found`
+        });
+    }
+};
+server.use(router);
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
-
-// Export the Server API
-module.exports = server
+    console.log('JSON Server is running');
+});
+module.exports = server;
